a939fcddba70d541390b1e38238e6b6dfc867769
Azan-10 <justjutt12@gmail.com>
2024-04-20 10:28:28 +0500

    Fist file

---
+++ main.ts
@@ -0,0 +1,14 @@
+// global scope
+let age = 18;
+if(true){
+console.log(age) // can use anywhere in the program
+}
+// LOCAL SCOPE
+let age1 = 18;
+if(true){   
+    
+let firstName ="AZAN" // cant use out side the block :block is the curly brackets
+console.log(firstName) 
+}
+// hoisting
+
