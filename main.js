43a13bed9ee0fca555357e296c1e7dce698c2b00
Azan-10 <justjutt12@gmail.com>
2024-04-20 11:11:35 +0500

    first coommit

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// global scope
-let age = 18;
-if (true) {
-    console.log(age); // can use anywhere in the program
-}
-// LOCAL SCOPE
-let age1 = 18;
-if (true) {
-    let firstName = "AZAN"; // cant use out side the block :block is the curly brackets
-    console.log(firstName);
-}
-// hoisting :> in hoisting variable declaration goes on top thats why we dont use var
-console.log("hello world");
-var age2 = 22;
-console.log(age2);
+// // global scope
+// let age = 18;
+// if (true) {
+//     console.log(age) // can use anywhere in the program
+// }
+// // LOCAL SCOPE
+// let age1 = 18;
+// if (true) {
+export {};
--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
-// global scope
-let age = 18;
-if(true){
-console.log(age) // can use anywhere in the program
-}
-// LOCAL SCOPE
-let age1 = 18;
-if(true){   
-    
-let firstName ="AZAN" // cant use out side the block :block is the curly brackets
-console.log(firstName) 
-}
-// hoisting :> in hoisting variable declaration goes on top thats why we dont use var
+// // global scope
+// let age = 18;
+// if (true) {
+//     console.log(age) // can use anywhere in the program
+// }
+// // LOCAL SCOPE
+// let age1 = 18;
+// if (true) {
 
-console.log("hello world")
-var age2 =22
-console.log(age2)
+//     let firstName = "AZAN" // cant use out side the block :block is the curly brackets
+//     console.log(firstName)
+// }
+// // hoisting :> in hoisting variable declaration goes on top thats why we dont use var
+
+// console.log("hello world")
+// var age2 = 22
+// console.log(age2)
+// // objects
+// let examsheet = {
+//     name: "hamzah",
+//     rollNumber: 1234,
+//     subject: "computer"
+// };
+
+// let message = `my name is: ${examsheet.name} and my roll number is: ${examsheet.rollNumber} and my subject is : ${examsheet.subject}`
+// console.log(message)
+
+import inquirer from "inquirer";
