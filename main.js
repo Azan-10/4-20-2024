c5d33f077acd345d2bcf8b331b3e48af139d265c
Azan-10 <justjutt12@gmail.com>
2024-04-20 10:28:40 +0500

    first file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // global scope
 let age = 18;
 if (true) {
@@ -10,4 +11,7 @@ if (true) {
     let firstName = "AZAN"; // cant use out side the block :block is the curly brackets
     console.log(firstName);
 }
-// hoisting
+// hoisting :> in hoisting variable declaration goes on top thats why we dont use var
+console.log("hello world");
+var age2 = 22;
+console.log(age2);
--- main.ts
+++ main.ts
@@ -10,5 +10,8 @@ if(true){
 let firstName ="AZAN" // cant use out side the block :block is the curly brackets
 console.log(firstName) 
 }
-// hoisting
+// hoisting :> in hoisting variable declaration goes on top thats why we dont use var
 
+console.log("hello world")
+var age2 =22
+console.log(age2)
